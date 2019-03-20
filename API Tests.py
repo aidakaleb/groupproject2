65c97a32795ec2532393b1fd85cc8bd205fb3e13
Devan Sood <dsood1@babson.edu>
2019-03-20 10:39:30 -0400

    Final
    includes G Ratings

--- API Tests.py
+++ API Tests.py
@@ -34,6 +34,7 @@ response_text2 = y.read().decode('utf-8')
 response_data_2 = json.loads(response_text2)
 pprint(response_data_2)
 Stop_Name = (response_data_2['data'][0]["attributes"]['name'])
+print(Stop_Name)
 Wheelchair_accessible = (response_data_2['data'][0]["attributes"]['wheelchair_boarding'])
 print('The Closest Stop To You is: ',Stop_Name)
 if Wheelchair_accessible == 1:
@@ -42,3 +43,15 @@ elif Wheelchair_accessible == 2:
     print('Wheelchairs Not Accepted')
 else:
     print("No Information Found")
+
+#GOOGLE MAPS
+SEARCHINPUT = 'MBTA%20stop%20near%20'+Stop_Name.replace(' ', '%20')
+GAPIKEY = 'AIzaSyCDLTn0Slx13crZWXxkXKHlTZxWys45X5U'
+GURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key={}".format(SEARCHINPUT, GAPIKEY)
+print(GURL)
+h = urllib.request.urlopen(GURL)
+response_text3 = h.read().decode('utf-8')
+response_data_3 = json.loads(response_text3)
+pprint(response_data_3)
+Rating = (response_data_3['candidates'][0]['rating'])
+print(Rating)
